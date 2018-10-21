1c2bd8bbdde235ebf16834d5ab21609fa6d75602
rdp1070 <rdp1070@rit.edu>
2018-10-21 14:50:09 -0400

    Push

---
+++ Civility Game Jam/Assets/_Scripts/ShapeChunkController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeChunkController : MonoBehaviour {
+
+    public BoxCollider2D Box;
+    public SpriteMask SpriteMask;
+    public bool isChipped;
+
+    public void SwitchOff() {
+        SpriteMask.enabled = true;
+        Box.enabled = false;
+        isChipped = true;
+    }
+    
+      public void SwitchOn()
+    {
+        SpriteMask.enabled = false;
+        Box.enabled = true;
+        isChipped = false;
+    }
+
+}
---
+++ Civility Game Jam/Assets/_Scripts/ShapeGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeGenerator : MonoBehaviour {
+
+    public Sprite sprite;
+    public SpriteRenderer spriteRenderer;
+    public ShapeChunkController[] shapeChunks;
+
+    /// <summary>
+    /// how many times we are cutting the sprite
+    /// </summary>
+    public float resolution;
+
+
+	// Use this for initialization
+	void Start () {
+        Initialize();
+	}
+
+    private void Initialize()
+    {
+        // at the beginning you need to determine which prefab of shapes you are and then make the
+        // triangles that fit inside of it.
+
+        shapeChunks = GetComponents<ShapeChunkController>();
+       
+    }
+
+}
